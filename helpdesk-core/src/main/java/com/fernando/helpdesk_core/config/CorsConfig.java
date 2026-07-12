05c9d5940f70b7a42b2e2b9a4f12c1f65d3aed50
FERNANDO <jorge.sierra@estudiantes.utec.edu.uy>
2026-07-12 19:12:46 -0300

    Agregar CORS para frontend en producción (Firebase)

--- helpdesk-core/src/main/java/com/fernando/helpdesk_core/config/CorsConfig.java
+++ helpdesk-core/src/main/java/com/fernando/helpdesk_core/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // puerto default de Vite
+                        .allowedOrigins("http://localhost:5173", "https://helpdesk-core-front.web.app")// puerto default de Vite
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
